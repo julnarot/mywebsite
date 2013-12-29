04226d4de359d3072708e9ebe1d707534082555a
jonatan <jonatan@jonatan-PC>
2013-12-29 09:16:22 -0500

    probandoservidordown

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     </head>
     <body>
         <p><a href="practica/matematica/calculadora/calculadora.html">Calculadora elemental</a></p>
+        <p align="center"><h1>hola practica</h1></p>
 <p>
     
    <iframe src="http://player.vimple.ru/iframe/de4a9b9886d8499c92d80c64b5538e42" width="650" height="400" frameborder="0" style="z-index:2147483647;"></iframe>
